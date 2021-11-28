97af10bf03edcca4516223ec2d927e5a4b73353b
urnisha19 <urnisha09@gmail.com>
2021-11-28 15:18:02 +0600

    Install react-bootstrap, create logo, Navigation, nav menu and setting up logo position

--- src/Components/MultiSharedComponents/NavBar/NavBar.js
+++ src/Components/MultiSharedComponents/NavBar/NavBar.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './NavBar.css';
+import { Container, Nav, Navbar, Button } from 'react-bootstrap';
+import logo from '../../../Images/logo.png'
 
 const NavBar = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Navbar bg="***" expand="lg">
+                <a>
+                    <img src={logo} alt="" className="logo w-50 px-3"/>
+                </a>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                <a className="navBar-link">Home</a>
+                <a className="navBar-link">About Us</a>
+                <a className="navBar-link">Services</a>
+                <a className="navBar-link">Our Psychotherapist </a>
+                <a className="navBar-link">Your Dashboard</a>
+                <a className="navBar-link">Admin</a>
+                <a className="navBar-link">Contact Us</a>
+                <a className="navBar-link">  <Button className="navBar-button">Login</Button></a>
+                </Nav>
+                </ Navbar.Collapse>
+            </Navbar>
+        </Container>
     );
 };
 
