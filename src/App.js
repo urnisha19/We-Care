48711f22cbe9f7d2ef9b6d5754ad7e0cd4e1d843
urnisha19 <urnisha09@gmail.com>
2021-11-28 22:55:05 +0600

    all routes and navigation set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,84 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Home from './Components/HomePage/Home';
+import AboutUs from './Components/AboutUsPage/AboutUs';
+import ServicePage from './Components/ServicesPage/ServicePage';
+import PsychotherapistPage from './Components/OurPsychotherapistPage/PsychotherapistPage';
+import MyAppointments from './Components/DashboardPage/MyAppointments/MyAppointments';
+import AddReview from './Components/DashboardPage/AddReview/AddReview';
+import AllAppointment from './Components/AdminPage/AllApointment/AllAppointment';
+import AddService from './Components/AdminPage/AddService/AddService';
+import ContactUs from './Components/ContactUsPage/ContactUs';
+import Login from './Components/LoginPage/Login';
+import NotFoundPage from './Components/NotFound/NotFoundPage';
 
 function App() {
   return (
     <div>
-      <Home></Home>
+      <Router>
+        <Switch>
+          {/* Home Page */}
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+
+          {/* About Us Page */}
+          <Route path="/aboutUs">
+            <AboutUs></AboutUs>
+          </Route>
+
+          {/*Our Services Page */}
+          <Route path="/ourServices">
+            <ServicePage></ServicePage>
+          </Route>
+
+          {/* Our Psychotherapists Page */}
+          <Route path="/ourPsychotherapist">
+            <PsychotherapistPage></PsychotherapistPage>
+          </Route>
+
+          {/* Your Dashboard Page */}
+          <Route path="dashboard/myAppointments">
+            <MyAppointments></MyAppointments>
+          </Route>
+          <Route path="dashboard/addReview">
+            <AddReview></AddReview>
+          </Route>
+
+          {/* Admin Page */}
+          <Route path="admin/allAppointments">
+            <AllAppointment></AllAppointment>
+          </Route>
+          <Route path="admin/addService">
+            <AddService></AddService>
+          </Route>
+
+          {/* Contact Us Page */}
+          <Route path="/contactUs">
+            <ContactUs></ContactUs>
+          </Route>
+
+          {/* Login Page */}
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+
+          {/* Route Not Page */}
+          <Route path="*">
+            <NotFoundPage></NotFoundPage>
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Components/MultiSharedComponents/NavBar/NavBar.js
+++ src/Components/MultiSharedComponents/NavBar/NavBar.js
@@ -2,26 +2,27 @@ import React from 'react';
 import './NavBar.css';
 import { Container, Nav, Navbar, Button } from 'react-bootstrap';
 import logo from '../../../Images/logo.svg'
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <Container>
             <Navbar bg="***" expand="lg">
-                <a>
-                    <img src={logo} alt="" className="logo w-50 px-3"/>
-                </a>
+                <Link>
+                    <img src={logo} alt="" className="logo w-50 px-3" />
+                </Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                <a className="navBar-link">Home</a>
-                <a className="navBar-link">About Us</a>
-                <a className="navBar-link">Services</a>
-                <a className="navBar-link">Our Psychotherapist </a>
-                <a className="navBar-link">Your Dashboard</a>
-                <a className="navBar-link">Admin</a>
-                <a className="navBar-link">Contact Us</a>
-                <a className="navBar-link">  <Button className="navBar-button">Login</Button></a>
-                </Nav>
+                    <Nav className="ml-auto">
+                        <Link path="/home" className="navBar-link">Home</Link>
+                        <Link path="/aboutUs" className="navBar-link">About Us</Link>
+                        <Link path="/ourServices" className="navBar-link">Services</Link>
+                        <Link path="/ourPsychotherapist" className="navBar-link">Our Psychotherapist </Link>
+                        <Link path="dashboard/myAppointments" className="navBar-link">Your Dashboard</Link>
+                        <Link path="admin/allAppointments" className="navBar-link">Admin</Link>
+                        <Link path="/contactUs" className="navBar-link">Contact Us</Link>
+                        <Link path="/login" className="navBar-link">  <Button className="navBar-button">Login</Button></Link>
+                    </Nav>
                 </ Navbar.Collapse>
             </Navbar>
         </Container>
